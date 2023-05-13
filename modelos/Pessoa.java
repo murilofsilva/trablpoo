9dbd1f1a2ad9697faef2ad4d50a45fda3b03e629
Jonathas <jonathasborges12@gmail.com>
2023-05-13 15:26:19 -0400

    Adicionando atributos em Pessoa.

--- modelos/Pessoa.java
+++ modelos/Pessoa.java
@@ -1,4 +1,9 @@
 package modelos;
+import java.time.LocalDate;
 
 public abstract class Pessoa {
+    protected String nome;
+    protected String cpfCnpj;
+    protected LocalDate dataNascimento;
+
 }
